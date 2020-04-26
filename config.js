27255d9e1ed4f5f583980075420ce80ec03e1604
groverkartik1998 <kartikgrover1998.kg@gmail.com>
2020-04-26 16:04:37 +0530

    setup start script and heroku port

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const secret = Math.random().toString(36).substring(7);
+const port = process.env.PORT || 8082;
 
 module.exports = {
     secret,
     lobbyTimeout: 10000,
     lobbyTimeoutCheckInterval: 5000,
-    port: 8081,
+    port: port,
     baseUrl: '/'       //For use with reverse proxies.
 };
