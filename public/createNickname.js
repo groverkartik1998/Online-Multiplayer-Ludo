e9bdabf613bb3cad312a80c740af80370c359cdb
groverkartik1998 <kartikgrover1998.kg@gmail.com>
2020-04-27 13:08:22 +0530

    changes

--- public/createNickname.js
+++ public/createNickname.js
@@ -1,5 +1,7 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    $(".g-signin2").css("display", "none");
+    $(".data").css("display", "block");
 }
 
 function signOut() {
