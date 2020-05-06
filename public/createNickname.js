0fca0c3e0fea4c2abd8655a47f8a86f69eb9bc99
groverkartik1998 <kartikgrover1998.kg@gmail.com>
2020-05-06 18:59:25 +0530

    changes

--- public/createNickname.js
+++ public/createNickname.js
@@ -7,6 +7,7 @@ function onSignIn(googleUser) {
     inputF.value = name; 
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log(result);
       }).catch(function(error) {
         
       });
