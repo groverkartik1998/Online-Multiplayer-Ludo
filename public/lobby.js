573a3bc647eec3f063c538b131baeea582e94e22
kartik grover <kartikgrover1998.kg@gmail.com>
2020-05-26 16:36:27 +0530

    changes

--- public/lobby.js
+++ public/lobby.js
@@ -157,18 +157,23 @@ function updateReadyButton() {
     }
 }
 
+function onLoad() {       
+    gapi.load('auth2', function() {         
+        gapi.auth2.init();       
+    });     
+}
+
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-    auth2.disconnect();
 }
 
 function logOut() {
     signOut();
-    localStorage.token = undefined;
-    window.location.href = baseUrl;
+    // localStorage.token = undefined;
+    // window.location.href = baseUrl;
 }
 
 $(document).ready(function () {
